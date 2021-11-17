2bdc49625e353242f7abe81d3b26929f38e367bb
rzkzx <romanzidanramadhan@gmail.com>
2021-11-17 16:07:07 +0800

    Add Profile Route

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -20,7 +20,8 @@
 
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#">Profile</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a
+                    href="{{ route('profile.show') }}">Profile</a></div>
         </div>
     </div>
 </div>
